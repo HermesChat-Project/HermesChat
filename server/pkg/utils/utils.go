eace958a64d90157509a07acb97c6d42e5b131f7
Gabriele <marellogabriele@gmail.com>
2023-02-17 12:10:31 +0100

    Login with Mongodb, token and server moved from http to https

--- server/pkg/utils/utils.go
+++ server/pkg/utils/utils.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/crypto/bcrypt"
 
 	"chat/pkg/config"
 )
@@ -23,8 +26,9 @@ func CreateToken (username string, c *gin.Context) {
 		fmt.Println(err)
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Error while creating token",
-			"error": err.Error(),
 		})
+		go config.WriteFileLog(err)
+		return
 	}
 
 	c.SetSameSite(http.SameSiteLaxMode)
@@ -32,6 +36,40 @@ func CreateToken (username string, c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"ris": "Token created",
 	})
-
 }
+
+func LoginMongoDB (username string, password string, c *gin.Context) {
+	if config.ClientMongoDB == nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error while connecting to database",
+		})
+		return
+	}
+	collection := config.ClientMongoDB.Database("user").Collection("user")
+	if collection == nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error while connecting to database",
+		})
+		return
+	}
+	ris := collection.FindOne(context.Background(), bson.M{"nickname": username})
+	var result bson.M
+	ris.Decode(&result)
+	if result == nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "Username not found",
+		})
+		return
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(result["password"].(string)), []byte(password))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "Wrong password",
+		})
+		return
+	}
+	CreateToken(username, c)
+	
+}
+
 
