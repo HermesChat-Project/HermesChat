e1716841cd149ee6bded5f60236af84cade6a60f
MolinoSimone <s.molino.1767@vallauri.edu>
2023-03-03 11:06:10 +0100

    Add other user for trial defined 3 others collection and collection friendship done

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { PersonalComponent } from './chat-selector/choose-chat/calendar/personal
 import { GroupComponent } from './chat-selector/choose-chat/calendar/group/group.component';
 import { PersonalEventComponent } from './chat-selector/choose-chat/calendar/personal/personal-event/personal-event.component';
 import { GroupEventComponent } from './chat-selector/choose-chat/calendar/group/group-event/group-event.component';
+import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -28,7 +29,8 @@ import { FormsModule } from '@angular/forms';
     PersonalComponent,
     GroupComponent,
     PersonalEventComponent,
-    GroupEventComponent
+    GroupEventComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
