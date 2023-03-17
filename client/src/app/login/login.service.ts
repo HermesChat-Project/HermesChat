58824381d248001ff18dd124f57b09aae687ba6e
Paolo Barbero <p.barbero.1701@vallauri.edu>
2023-03-17 20:28:54 +0100

    fixed some problems and started the styling of the text

--- client/src/app/login/login.service.ts
+++ client/src/app/login/login.service.ts
@@ -9,9 +9,10 @@ export class LoginService {
   constructor(private dataStorage: DataStorageService) {}
 
   login(body: any, options: any) {
-    this.dataStorage.PostRequestWithOptions("login", body, options).subscribe(
+    this.dataStorage.PostRequestWithOptions(`login?username=${body.username}&password=${body.password}`, body, options).subscribe(
       (response: any) => {
         console.log(response);
+        this.loggedIn = true;
       },
       (error: Error) => {
         console.log(error);
