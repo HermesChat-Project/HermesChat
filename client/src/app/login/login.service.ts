10c0ed402f8b681680ce7448ce49ad9b91fc3fb5
Paolo Barbero <p.barbero.1701@vallauri.edu>
2023-03-26 13:45:45 +0200

    tranlations implemented

--- client/src/app/login/login.service.ts
+++ client/src/app/login/login.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { TranslationsService } from '../shared/translations.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
   loggedIn: boolean = false;
-  constructor(private dataStorage: DataStorageService) {}
+  constructor(private dataStorage: DataStorageService, private translationsService: TranslationsService) {}
 
-  login(body: any, options: any) {
+  login(body: any, options: any, errors: HTMLElement) {
     this.dataStorage.PostRequestWithOptions(`login?username=${body.username}&password=${body.password}`, body, options).subscribe(
       (response: any) => {
         console.log(response);
@@ -16,6 +17,7 @@ export class LoginService {
       },
       (error: Error) => {
         console.log(error);
+        errors.innerHTML = this.translationsService.languageWords["login"]["wrong-credentials"];
       }
     );
   }
